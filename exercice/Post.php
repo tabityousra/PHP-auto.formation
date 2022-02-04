b82b9ec5fbafac8fc8b38e34ea52bbd3906869d2
tabityousra <92039920+tabityousra@users.noreply.github.com>
2022-02-04 13:25:43 +0100

    add

---
+++ exercice/Post.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "Your first name is " . $_POST['first name'] . "<br/>" ;
+    echo "Your second name is " . $_POST['second'] ."<br/>"  ;
+    echo "Your Email is " . $_POST['email'] ."<br/>"  ;
+    echo "Your Phone number is " . $_POST['phone'] ."<br/>"  ;
