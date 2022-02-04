ea1717b7bd2c28d487936124d6174a770039773d
tabityousra <92039920+tabityousra@users.noreply.github.com>
2022-02-04 10:10:55 +0100

    add

--- Array/array.php
+++ Array/array.php
@@ -4,3 +4,8 @@ $cars = array("Volvo", "BMW", "Toyota");
 echo "cars name : " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 ?>
 
+
+<?php
+$student = array("nada", "youssra","sara");
+echo "student name is ".$student[1];
+?>
